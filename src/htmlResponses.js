60d64c02ccf85a87a261198b955809b3dc615e2c
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-21 21:52:48 -0500

    File System Module setup Require FS and use it to read client.html, style.css, and bundle.js into memory

---
+++ src/htmlResponses.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
+const css = fs.readFileSync(`${__dirname}/../hosted/style.css`);
+const bundle = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
