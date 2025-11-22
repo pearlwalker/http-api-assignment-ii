3cf4892ba699adfbf74267952e7bac25e7dbbaef
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-21 21:48:22 -0500

    Developed onRequest

--- src/server.js
+++ src/server.js
@@ -9,7 +9,12 @@ const urlStruct = {
 };
 
 const onRequest = (req, res) => {
+    const protocol = req.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(req.url, `${protocol}://${req.headers.host}`);
 
+    if(urlStruct[parsedUrl.pathname]) {
+        urlStruct[parsedUrl.pathname](req, res);
+    };
 };
 
 http.createServer(onRequest).listen(port, () => {
