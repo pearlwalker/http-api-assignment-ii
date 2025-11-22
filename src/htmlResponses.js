cb4959fa26a9418edaf302a4749a9c4038e6de22
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-21 21:54:24 -0500

    Created empty functions for file handling

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,3 +3,19 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 const css = fs.readFileSync(`${__dirname}/../hosted/style.css`);
 const bundle = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
+
+const serveFile = (res, file, contentType) => {
+
+};
+
+const getIndex = (req, res) => {
+
+};
+
+const getCSS = (req, res) => {
+
+};
+
+const getBundle = (req, res) => {
+
+};
