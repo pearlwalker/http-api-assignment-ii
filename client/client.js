a75e72b4f53eb875dda4ea7edf876624c67d5680
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-22 18:33:19 -0500

    Added reference to getUsers form

--- client/client.js
+++ client/client.js
@@ -8,6 +8,7 @@ const sendRequest = async (form) => {
 
 const init = () => {
     const addForm = document.querySelector('#form-addUser');
+    const getForm = document.querySelector('form-getUsers');
 };
 
 window.onload = init;
