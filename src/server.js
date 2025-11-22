29755e2bc104299e25ea79334cb42d2e86591215
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-21 21:40:39 -0500

    Created port constant

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
