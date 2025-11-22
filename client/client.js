35a2558e38f1f42b9f076553c4e34a72657a3885
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-22 18:44:39 -0500

    For POST requests, add name and age to the request body

--- client/client.js
+++ client/client.js
@@ -5,6 +5,15 @@ const handleResponse = async (res) => {
 const sendRequest = async (form) => {
     const url = form.getAttribute('action');
     const method = form.getAttribute('method');
+    
+    let reqBody;
+
+    if( method === 'POST' || method === 'post') {
+        const name = form.querySelector('#nameField').value;
+        const age = form.querySelector('#ageField').value;
+
+        reqBody = `name=${name}&age=${age}`;
+    };
 };
 
 const init = () => {
